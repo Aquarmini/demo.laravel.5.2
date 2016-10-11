831d772a46d72c22578c6b5a4bbfe8894d2404ac
李铭昕 <715557344@qq.com>
2016-10-11 10:19:28 +0800

    * 修改js测试

--- resources/views/index/demo/js.blade.php
+++ resources/views/index/demo/js.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.master')
 @section('content')
+    <code>
+        <pre>
+        aa();
+        function aa() {
+            var a = {
+                "val": 1
+            };
+            conLog(a);bb(a);conLog(a);
+
+            var b = 1;
+            conLog(b);cc(b);conLog(b);
+        }
+
+        function bb(a) {
+            ++a.val
+            conLog(a);
+        }
+
+        function cc(a) {
+            ++a;
+            conLog(a);
+        }
+        </pre>
+
+    </code>
     <script>
         aa();
         function aa() {
