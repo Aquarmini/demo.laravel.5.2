804d5db9b2dd4a9135678e01d9ec1600b54e24ea
李铭昕 <715557344@qq.com>
2016-10-14 15:51:29 +0800

    * 增加pdo测试

--- app/Http/Controllers/Index/DemoController.php
+++ app/Http/Controllers/Index/DemoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Ajax;
+use Helper;
 
 class DemoController extends Controller
 {
@@ -141,10 +142,19 @@ class DemoController extends Controller
 
     public function getMypdo()
     {
-        $pdo = \limx\tools\MyPDO::getInstance();
-        $sql = 'select * from test where id < ?;';
+        $pdo = Helper::pdo();
+        $sql = 'select * from user where id < ?;';
         $res = $pdo->query($sql, [4]);
         dump($res);
+
+        $pdo->beginTransaction();
+        $sql = 'insert into user(username) values(?);';
+        $time = time();
+        $res = $pdo->execute($sql, [$time]);
+        dump($res);
+        $pdo->commit();
+//        $pdo->rollback();
+
     }
 
 }
--- app/Services/HelperService.php
+++ app/Services/HelperService.php
@@ -12,4 +12,16 @@ class HelperService
     {
         return $this->root . $path;
     }
+
+    public function pdo()
+    {
+        $config['host'] = env('DB_HOST', '127.0.0.1');
+        $config['user'] = env('DB_USERNAME', 'root');
+        $config['pwd'] = env('DB_PASSWORD', '');
+        $config['dbname'] = env('DB_DATABASE', 'test');
+        $config['host'] = env('DB_HOST', '127.0.0.1');
+        $config['charset'] = 'utf8';
+
+        return \limx\tools\MyPDO::getInstance($config);
+    }
 }
